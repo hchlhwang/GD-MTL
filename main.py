41448c8ba6ee1fd4efe49858d96aebcd0da8014e
hchlhwang <hchlhwang@gmail.com>
2022-06-21 22:52:04 -0400

    SegNet added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import torch
+import torch.optim as optim
 from model import SegNet
 from util import parseTag
 
@@ -7,11 +8,57 @@ def main():
 
   parseTag()
 
-  # Set GPU
-  device = torch.cuda.device('cuda:0' if torch.cuda.is_available else 'cpu')
+  # Set GPU in desktop or mac m1
+  if torch.cuda.is_available():
+    device = torch.cuda.device('cuda:0' if torch.cuda.is_available else 'cpu')
+  else:
+    device = torch.device("mps")
+
+  print(f"Using GPS device: {device}")
 
   # Send model to GPU
   model = SegNet().to(device)
+
+  # Optimze params
+  optimizer = optim.Adam(SegNet.parameters(), lr=1e-4)
+  scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.5)
+
+  print('Parameter Space: ABS: {:.1f}, REL: {:.4f}'.format(count_parameters(SegNet),
+                                                           count_parameters(SegNet) / 24981069))
+  print('LOSS FORMAT: SEMANTIC_LOSS MEAN_IOU PIX_ACC | DEPTH_LOSS ABS_ERR REL_ERR | NORMAL_LOSS MEAN MED <11.25 <22.5 <30')
+
+
+  # define dataset
+  dataset_path = opt.dataroot
+  if opt.apply_augmentation:
+      nyuv2_train_set = NYUv2(root=dataset_path, train=True, augmentation=True)
+      print('Applying data augmentation on NYUv2.')
+  else:
+      nyuv2_train_set = NYUv2(root=dataset_path, train=True)
+      print('Standard training strategy without data augmentation.')
+
+  nyuv2_test_set = NYUv2(root=dataset_path, train=False)
+
+  batch_size = 2
+  nyuv2_train_loader = torch.utils.data.DataLoader(
+      dataset=nyuv2_train_set,
+      batch_size=batch_size,
+      shuffle=True)
+
+  nyuv2_test_loader = torch.utils.data.DataLoader(
+      dataset=nyuv2_test_set,
+      batch_size=batch_size,
+      shuffle=False)
+
+  # Train and evaluate single-task network
+  single_task_trainer(nyuv2_train_loader,
+                      nyuv2_test_loader,
+                      SegNet,
+                      device,
+                      optimizer,
+                      scheduler,
+                      opt,
+                      200)
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python -tt
 """
   Implementation of SegNet, single-task
 
@@ -11,90 +12,122 @@
         3) ReLU
 """
 import torch.nn as nn
+import torch.nn.functional as F
 from util import parseTag
 
 class SegNet(nn.Module):
+    def __init__(self):
+        super(SegNet, self).__init__()
+        # Network params
+        vggFilters = [64, 128, 256, 512, 512]
+        self.classNum = 13
 
-  def __init__(self):
-    super(SegNet, self).__init__()
 
-    # Network params
-    vggFilters = [64, 128, 256, 512, 512]
-    self.classNum = 13
+        # Initialize enc/decoder block
+        self.encoderBlock = nn.ModuleList([self.subBlock([3, vggFilters[0]])])
+        self.decoderBlock = nn.ModuleList([self.subBlock([vggFilters[-1]])])
 
-    # Initialize enc/decoder block
-    self.encoderBlock = nn.ModuleList([self.subBlock([3, vggFilters[0]])])
-    self.decoderBlock = nn.ModuleList([self.subBlock([vggFilters[-1]])])
+        # Construct rest of the blocks
+        for i in range(len(vggFilters)):
+            if i == 0:
+                self.encoderBlock.append(self.subBlock([vggFilters[i]]))
+                self.decoderBlock.insert(0, self.subBlock([vggFilters[-i-1]]))
+            elif i == 1:
+                self.encoderBlock.append(self.subBlock([vggFilters[i-1], vggFilters[i]]))
+                self.encoderBlock.append(self.subBlock([vggFilters[i]]))
+                self.decoderBlock.insert(0, self.subBlock([vggFilters[-i-1], vggFilters[-i]]))
+                self.decoderBlock.insert(0, self.subBlock([vggFilters[-i-1]]))
+            else:
+                self.encoderBlock.append(self.subBlock([vggFilters[i-1], vggFilters[i]]))
+                self.encoderBlock.append(self.subBlock([vggFilters[i]]))
+                self.encoderBlock.append(self.subBlock([vggFilters[i]]))
+                self.decoderBlock.insert(0, self.subBlock([vggFilters[-i-1], vggFilters[-i]]))
+                self.decoderBlock.insert(0, self.subBlock([vggFilters[-i-1]]))
+                self.decoderBlock.insert(0, self.subBlock([vggFilters[-i-1]]))
 
-    # Construct rest of the blocks
-    for i in range(len(vggFilters)):
-      if i == 0:
-        self.encoderBlock.append(self.subBlock([vggFilters[i]))
-        self.decoderBlock.insert(0, self.subBlock([vggFilters[-i-1]]))
-      elif i == 1:
-        self.encoderBlock.append(self.subBlock([vggFilters[i-1], vggFilters[i]]))
-        self.encoderBlock.append(self.subBlock([vggFilters[i]))
-        self.decoderBlock.insert(0, self.subBlock([vggFilters[-i-1], vggFilters[-i]]))
-        self.decoderBlock.insert(0, self.subBlock([vggFilters[-i-1]]))
-      else:
-        self.encoderBlock.append(self.subBlock([vggFilters[i-1], vggFilters[i]]))
-        self.encoderBlock.append(self.subBlock([vggFilters[i]))
-        self.encoderBlock.append(self.subBlock([vggFilters[i]))
-        self.decoderBlock.insert(0, self.subBlock([vggFilters[-i-1], vggFilters[-i]]))
-        self.decoderBlock.insert(0, self.subBlock([vggFilters[-i-1]]))
-        self.decoderBlock.insert(0, self.subBlock([vggFilters[-i-1]]))
-
-      # Prediction layer
-      if option.task == 'semantic':
+        # Prediction layer
+        if option.task == 'semantic':
           self.predLayer = self.subBlock([filter[0], self.classNum], pred=True)
-      if option.task == 'depth':
+        if option.task == 'depth':
           self.predLayer = self.subBlock([filter[0], 1], pred=True)
-      if option.task == 'normal':
+        if option.task == 'normal':
           self.predLayer = self.subBlock([filter[0], 3], pred=True)
 
-      # Pooling
-      self.downSample = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
-      self.upSample = nn.MaxPool2d(kernel_size=2, stride=2)
+        # Pooling
+        self.downSample = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
+        self.upSample = nn.MaxPool2d(kernel_size=2, stride=2)
 
-    # Decoder network (13 corresponding layers)
-    # Pixelwise classifcaiton layer
-    # First convolution blocks in the encoder network (layers 1, 3, 5, 8, 11)
+        # Decoder network (13 corresponding layers)
+        # Pixelwise classifcaiton layer
+        # First convolution blocks in the encoder network (layers 1, 3, 5, 8, 11)
 
-    # Initialize weights
-    initWeights()
+        # Initialize weights
+        initWeights()
 
-  # Sublock contains: conv + batch norm + ReLU
-  def subBlock(self, Filter, pred=False):
-    if len(Filter) == 2: # Encoder/Decoder block
-      block = nn.Sequential(
-        nn.Conv2d(in_channels=seqFilters[0], out_channels=seqFilters[1], kernel_size=3, padding=1),
-        nn.BatchNorm2d(num_features=seqFilters[1]),
-        nn.ReLU(),
-      )
-    else: # Conv/deconvolution block
-      if pred:
-        block = nn.Sequential(
-          nn.Conv2d(in_channels=seqFilters[0], out_channels=seqFilters[0], kernel_size=3, padding=1),
-          nn.Conv2d(in_channels=seqFilters[0], out_channels=seqFilters[1], kernel_size=1, padding=0),
-        )
-      else:
-        block = nn.Sequential(
-          nn.Conv2d(in_channels=seqFilters[0], out_channels=seqFilters[0], kernel_size=3, padding=1),
-          nn.BatchNorm2d(num_features=seqFilters[1]),
-          nn.ReLU(),
-        )
-    return block
+    # Sublock contains: conv + batch norm + ReLU
+    def subBlock(self, Filter, pred=False):
+        if len(Filter) == 2: # Encoder/Decoder block
+            block = nn.Sequential(
+            nn.Conv2d(in_channels=seqFilters[0], out_channels=seqFilters[1], kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_features=seqFilters[1]),
+            nn.ReLU(),
+            )
+        else: # Conv/deconvolution block
+            if pred:
+                block = nn.Sequential(
+                  nn.Conv2d(in_channels=seqFilters[0], out_channels=seqFilters[0], kernel_size=3, padding=1),
+                  nn.Conv2d(in_channels=seqFilters[0], out_channels=seqFilters[1], kernel_size=1, padding=0),
+                )
+            else:
+                block = nn.Sequential(
+                  nn.Conv2d(in_channels=seqFilters[0], out_channels=seqFilters[0], kernel_size=3, padding=1),
+                  nn.BatchNorm2d(num_features=seqFilters[1]),
+                  nn.ReLU(),
+                )
+        return block
 
-  def initWeights(self):
-    for m in self.Modules():
-      if isinstance(m, nn.Conv2d):
-        nn.init.xavier_normal_(m.weight)
-        nn.init.constant_(m.bias, 0)
-      elif isinstance(m, nn.Conv2d):
-        nn.init.constant_(m.weight, 1)
-        nn.init.constant_(m.bias, 0)
-      elif isinstance(m, nn.Conv2d):
+    def initWeights(self):
+        for m in self.Modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.xavier_normal_(m.weight)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                nn.init.constant_(m.bias, 0)
 
+    def forward(self, x):
+    	indices = [0] * 5
 
+    	# Forword encoder block
+    	for i in range(5):
+    		if i < 2:
+    			x = self.encoderBlock[2*i+1](self.encoderBlock[2*i](x))
+    			x, indices[i] = self.downSample(x)
+    		else:
+    			x = self.encoderBlock[3*i-1](self.encoderBlock[3*i-2](x))
+    			x = self.encoderBlock[3*i](x)
+    			x, indices[i] = self.downSample(x)
 
-  def forward(self, x):
+    	# Forword decoder block
+    	for i in range(5):
+    		if i < 3:
+    			x = self.encoderBlock[3*i+1](self.encoderBlock[3*i](x))
+    			x = self.encoderBlock[3*i+2](x)
+    			x = self.upSample(x)
+    		else:
+    			x = self.encoderBlock[2*i+4](self.encoderBlock[2*i+3](x))
+    			x = self.upSample(x)
+
+        # Prediction layer
+        if option.task == 'semantic':
+            pred = F.log_softmax(self.predLayer(x), dim=1)
+        elif option.task == 'depth':
+            pred = self.predLayer(x)
+        elif option.task == 'normal':
+            pred = self.predLayer(x)
+        	pred = pred / torch.norm(pred, p=2, dim=1, keepdim=True)
+
+        return pred
